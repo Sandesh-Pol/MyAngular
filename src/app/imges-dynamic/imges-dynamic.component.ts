93492b3bc2439c6ef1c22a54ff3b4668f16f4533
Sandesh-Pol <sandeshpol268@gmail.com>
2023-06-07 09:35:41 +0530

    todays commit

--- src/app/imges-dynamic/imges-dynamic.component.ts
+++ src/app/imges-dynamic/imges-dynamic.component.ts
@@ -8,9 +8,9 @@ import { Component } from '@angular/core';
 export class ImgesDynamicComponent {
   url = "assets/img1.png"
   allImages = [
-    {img,'assets/img2.png'},
-    {img,'assets/img2.png'},
-    {img,'assets/images.png'},
+    {img:'assets/img2.png'},
+    {img:'assets/img2.png'},
+    {img:'assets/images.png'},
   ]
   constructor(){}
   changeImg(){
